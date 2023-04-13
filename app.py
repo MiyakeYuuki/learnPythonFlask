fcd3b47b97c4685365ad8d8c6da94fa97430d542
MiyakeYuuki <77323541+MiyakeYuuki@users.noreply.github.com>
2023-04-13 23:26:12 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -5,21 +5,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # flask-loginのLoginManagerとflaskアプリの紐付け
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notoapp.db'
-app.config['SECRET_KEY'] = os.urandom(24)
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notoapp.db'
+# app.config['SECRET_KEY'] = os.urandom(24)
+# db = SQLAlchemy(app)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
 
 # DBのテーブルに対応するUserクラス
 
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(25))
+# class User(UserMixin, db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(50), nullable=False, unique=True)
+#     password = db.Column(db.String(25))
 
 # 「/」へアクセスがあった場合に、"Hello World"の文字列を返す
 
